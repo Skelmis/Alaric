16fcff7f3f165b79dd6ce4313f70e5ebbc911369
skelmis <ethan@koldfusion.xyz>
2022-06-02 08:35:07 +1200

    feature: Rewrite Document to be concise and support AQ

--- alaric/document.py
+++ alaric/document.py
@@ -1,9 +1,8 @@
 import functools
-from copy import deepcopy
 from typing import List, Dict, Optional, Union, Any, TypeVar, Type
 
-from motor.motor_asyncio import AsyncIOMotorDatabase, AsyncIOMotorCollection
 from pymongo.results import DeleteResult
+from motor.motor_asyncio import AsyncIOMotorDatabase, AsyncIOMotorCollection
 
 from alaric.abc import BuildAble
 
@@ -12,9 +11,8 @@ T = TypeVar("T")
 
 def return_converted(func):
     """
-    If we have a registered converter,
-    this deco will attempt to parse
-    the given data into our provided
+    If we have a registered converter, this deco will
+    attempt to parse the given data into our provided
     class through the use of dictionary unpacking.
     """
 
@@ -55,7 +53,7 @@ class Document:
         document_name: str
             What this _document should be called
         converter: Optional[Type[T]]
-            An optional converter to try
+            An optional class to try
             to convert all data-types which
             return either Dict or List into
         """
@@ -70,10 +68,9 @@ class Document:
 
     @return_converted
     async def find(
-        self, filter_dict: Union[Dict, BuildAble]
+        self, filter_dict: Union[Dict[str, Any], BuildAble]
     ) -> Optional[Union[Dict[str, Any], Type[T]]]:
-        """
-        Find and return one item.
+        """Find and return one item.
 
         Parameters
         ----------
@@ -86,12 +83,8 @@ class Document:
         Optional[Union[Dict[str, Any], Type[T]]]
             The result of the query
         """
-        if isinstance(filter_dict, BuildAble):
-            filter_dict = filter_dict.build()
-
-        filter_dict = self.__convert_filter(filter_dict)
+        filter_dict = self.__ensure_built(filter_dict)
         self.__ensure_dict(filter_dict)
-
         return await self._document.find_one(filter_dict)
 
     @return_converted
@@ -113,11 +106,8 @@ class Document:
         List[Union[Dict[str, Any], Type[T]]]
             The result of the query
         """
-        if isinstance(filter_dict, BuildAble):
-            filter_dict = filter_dict.build()
-
+        filter_dict = self.__ensure_built(filter_dict)
         self.__ensure_dict(filter_dict)
-
         return await self._document.find(filter_dict).to_list(None)
 
     async def delete(
@@ -138,42 +128,12 @@ class Document:
         Optional[DeleteResult]
             The result of deletion if it occurred.
         """
-        filter_dict = self.__convert_filter(filter_dict)
+        filter_dict = self.__ensure_built(filter_dict)
         self.__ensure_dict(filter_dict)
-
         result: DeleteResult = await self._document.delete_many(filter_dict)
         result: Optional[DeleteResult] = result if result.deleted_count != 0 else None
         return result
 
-    async def update(
-        self,
-        filter_dict: Union[Dict, Any],
-        data: Dict[str, Any] = None,
-        *args: Any,
-        **kwargs: Any,
-    ) -> None:
-        """
-        Update an existing _document within the database
-        Parameters
-        ----------
-        filter_dict: Union[Dict, Any]
-            The _id of the item to update by,
-            if a Dict is passed that is
-            used as the filter.
-        data: Dict[str, Any]
-            The data we want to update with
-        """
-        filter_dict = self.__convert_filter(filter_dict)
-
-        if data is None:
-            # Backwards compat so you can just pass something like
-            # await doc.upsert({"_id": 1, "data": False})
-            data = deepcopy(filter_dict)
-            filter_dict = self.__convert_filter(data.pop("_id"))
-
-        await self.update_by_custom(filter_dict, data, *args, **kwargs)
-
-    # <-- Actual Methods -->
     @return_converted
     async def get_all(
         self,
@@ -207,210 +167,121 @@ class Document:
             The items matching the filter
         """
         filter_dict = filter_dict or {}
-        if isinstance(filter_dict, BuildAble):
-            filter_dict = filter_dict.build()
+        filter_dict = self.__ensure_built(filter_dict)
 
         return await self._document.find(filter_dict, *args, **kwargs).to_list(None)
 
     async def insert(self, data: Dict[str, Any]) -> None:
-        """
-        Insert the given data into the _document
+        """Insert the provided data into the document.
+
         Parameters
         ----------
         data: Dict[str, Any]
             The data to insert
         """
         self.__ensure_dict(data)
-
         await self._document.insert_one(data)
 
-    async def upsert(
+    async def update(
         self,
-        filter_dict: Union[Dict, Any],
-        data: Dict[str, Any] = None,
-        option: str = "set",
-        *args: Any,
-        **kwargs: Any,
-    ) -> None:
-        """
-        Performs an UPSERT operation,
-        so data is either INSERTED or UPDATED
-        based on the current state of the _document.
-        Parameters
-        ----------
-        filter_dict: Union[Dict, Any]
-            The _id of the item to update by,
-            if a Dict is passed that is
-            used as the filter.
-        data: Dict[str, Any]
-            The data to upsert (filter is _id)
-        option: str
-            The optional option to pass to mongo,
-            default is set
-        """
-        # Fairly sure this is no longer needed
-        # if await self.find_by_id(data["_id"]) is None:
-        #     return await self.insert(data)
-        filter_dict = self.__convert_filter(filter_dict)
-
-        if data is None:
-            # Backwards compat so you can just pass something like
-            # await doc.upsert({"_id": 1, "data": False})
-            data = deepcopy(filter_dict)
-            filter_dict = self.__convert_filter(data.pop("_id"))
-
-        await self.upsert_custom(filter_dict, data, option, *args, **kwargs)
-
-    async def update_by_id(
-        self, data: Dict[str, Any], option: str = "set", *args: Any, **kwargs: Any
-    ) -> None:
-        """
-        Performs an update operation.
-        Parameters
-        ----------
-        data: Dict[str, Any]
-            The data to upsert (filter is _id)
-        option: str
-            The optional option to pass to mongo,
-            default is set
-        Notes
-        -----
-        If the data doesn't already
-        exist, this makes no changes
-        to the actual database.
-        """
-        self.__ensure_dict(data)
-        self.__ensure_id(data)
-
-        data_id = data.pop("_id")
-        await self._document.update_one(
-            {"_id": data_id}, {f"${option}": data}, *args, **kwargs
-        )
-
-    async def upsert_custom(
-        self,
-        filter_dict: Dict[str, Any],
+        filter_dict: Union[Dict[str, Any], BuildAble],
         update_data: Dict[str, Any],
         option: str = "set",
         *args: Any,
         **kwargs: Any,
     ) -> None:
-        """
-        Performs an UPSERT operation,
-        so data is either INSERTED or UPDATED
-        based on the current state of the _document.
-        Uses filter_dict rather then _id
+        """Performs an UPDATE operation.
+
         Parameters
         ----------
-        filter_dict: Dict[str, Any]
+        filter_dict: Union[Dict[str, Any], BuildAble]
             The data to filter on
         update_data: Dict[str, Any]
             The data to upsert
         option: str
             The optional option to pass to mongo,
             default is set
+            https://www.mongodb.com/docs/manual/reference/operator/update/
         """
-        await self.update_by_custom(
-            filter_dict, update_data, option, upsert=True, *args, **kwargs
-        )
-
-    async def update_by_custom(
-        self,
-        filter_dict: Dict[str, Any],
-        update_data: Dict[str, Any],
-        option: str = "set",
-        *args: Any,
-        **kwargs: Any,
-    ) -> None:
-        """
-        Performs an update operation.
-        Parameters
-        ----------
-        filter_dict: Dict[str, Any]
-            The data to filter on
-        update_data: Dict[str, Any]
-            The data to upsert
-        option: str
-            The optional option to pass to mongo,
-            default is set
-        """
+        filter_dict = self.__ensure_built(filter_dict)
         self.__ensure_dict(filter_dict)
         self.__ensure_dict(update_data)
 
-        # Update
         await self._document.update_one(
             filter_dict, {f"${option}": update_data}, *args, **kwargs
         )
 
-    async def unset(self, _id: Union[Dict, Any], field: Any) -> None:
-        """
-        Remove a given param, basically dict.pop on the db.
-        Works based off _id
-        Parameters
-        ----------
-        _id: Any
-            The field's _document id or
-            dict as a filter
-        field: Any
-            The field to remove
-        """
-        filter_dict = self.__convert_filter(_id)
-        await self.unset_by_custom(filter_dict, field)
+    async def upsert(
+        self,
+        filter_dict: Union[Dict[str, Any], BuildAble],
+        update_data: Dict[str, Any],
+        option: str = "set",
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        """Performs an UPSERT operation.
 
-    async def unset_by_custom(self, filter_dict: Dict[str, Any], field: Any) -> None:
-        """
-        Remove a given param, basically dict.pop on the db.
-        Works based off _id
         Parameters
         ----------
-        filter_dict: Dict[str, Any]
+        filter_dict: Union[Dict[str, Any], BuildAble]
+            The data to filter on
+        update_data: Dict[str, Any]
+            The data to upsert
+        option: str
+            Update operator.
+            https://www.mongodb.com/docs/manual/reference/operator/update/
+        """
+        filter_dict = self.__ensure_built(filter_dict)
+        self.__ensure_dict(filter_dict)
+        self.__ensure_dict(update_data)
+        await self.update(
+            filter_dict, update_data, option, upsert=True, *args, **kwargs
+        )
+
+    async def unset(
+        self, filter_dict: Union[Dict[str, Any], BuildAble], field: Any
+    ) -> None:
+        """Delete a given field on a collection
+
+        Parameters
+        ----------
+        filter_dict: Union[Dict[str, Any], BuildAble]
             The fields to match on (Think _id)
         field: Any
             The field to remove
         """
+        filter_dict = self.__ensure_built(filter_dict)
         self.__ensure_dict(filter_dict)
         await self._document.update_one(filter_dict, {"$unset": {field: True}})
 
     async def increment(
-        self, data_id: Union[Dict, Any], amount: Union[int, float], field: str
+        self,
+        filter_dict: Union[Dict[str, Any], BuildAble],
+        field: str,
+        amount: Union[int, float],
     ) -> None:
-        """
-        Increment a field somewhere.
+        """Increment the provided field.
+
         Parameters
         ----------
-        data_id: Any
-            The fields to match on (Think _id)
-        amount: Union[int, float]
-            How much to increment (or decrement) by
-        field: str
-            The key for the field to increment
-        """
-        filter_dict = self.__convert_filter(data_id)
-
-        await self.increment_by_custom(filter_dict, amount, field)
-
-    async def increment_by_custom(
-        self, filter_dict: Dict[Any, Any], amount: Union[int, float], field: str
-    ) -> None:
-        """
-        Increment a field somewhere.
-        Parameters
-        ----------
-        filter_dict: Dict[Any, Any]
+        filter_dict: Union[Dict[str, Any], BuildAble]
             The 'thing' we want to increment
-        amount: Union[int, float]
-            How much to increment (or decrement) by
         field: str
             The key for the field to increment
+        amount: Union[int, float]
+            How much to increment (or decrement) by
         """
+        filter_dict = self.__ensure_built(filter_dict)
         self.__ensure_dict(filter_dict)
         await self._document.update_one(filter_dict, {"$inc": {field: amount}})
 
-    async def update_field_to(
-        self, filter_dict: Union[Dict[Any, Any], BuildAble], field: str, new_value: Any
+    async def change_field_to(
+        self,
+        filter_dict: Union[Dict[str, Any], BuildAble],
+        field: str,
+        new_value: Any,
     ) -> None:
-        """
-        Modify a single field and change the value.
+        """Modify a single field and change the value.
 
         Parameters
         ----------
@@ -422,9 +293,7 @@ class Document:
         new_value: Any
             What the field should get changed to
         """
-        if isinstance(filter_dict, BuildAble):
-            filter_dict = filter_dict.build()
-
+        filter_dict = self.__ensure_built(filter_dict)
         self.__ensure_dict(filter_dict)
         await self._document.update_one(filter_dict, {"$set": {field: new_value}})
 
@@ -450,6 +319,13 @@ class Document:
     @staticmethod
     def __ensure_dict(data: Dict[str, Any]) -> None:
         assert isinstance(data, dict)
+
+    @staticmethod
+    def __ensure_built(data: Union[Dict, BuildAble]) -> Dict:
+        if isinstance(data, BuildAble):
+            return data.build()
+
+        return data
 
     # <-- Some basic internals -->
     @property
