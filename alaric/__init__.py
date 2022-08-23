3fe0e6fcb688c2230e1fb13e0d322b7995927523
skelmis <ethan@koldfusion.xyz>
2022-08-24 03:37:49 +1200

    chore: update version

--- alaric/__init__.py
+++ alaric/__init__.py
@@ -5,7 +5,7 @@ from .advanced_query import AQ
 from .document import Document
 
 __all__ = ("AQ", "Document")
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=1, minor=1, micro=3, releaselevel="final", serial=0)
+version_info = VersionInfo(major=1, minor=1, micro=4, releaselevel="final", serial=0)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
