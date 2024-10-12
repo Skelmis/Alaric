7405330bbf891a80722ba78c27ecb88efa61ba89
skelmis <skelmis.craft@gmail.com>
2024-10-13 01:33:09 +1300

    chore: bump version

--- alaric/__init__.py
+++ alaric/__init__.py
@@ -20,7 +20,7 @@ __all__ = (
     "util",
     "CachedDocument",
 )
-__version__ = "1.4.0"
+__version__ = "1.5.0"
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=1, minor=4, micro=0, releaselevel="final", serial=0)
+version_info = VersionInfo(major=1, minor=5, micro=0, releaselevel="final", serial=0)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
